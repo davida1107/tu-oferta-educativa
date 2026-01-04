ab5369944b6998848dfe393b59e8a3d06f22461e
davida1107 <davidavendano@politecnicomayor.edu.co>
2026-01-04 15:27:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const programas = [
-    // --- ESCUELA DE SALUD (7 Programas) ---
     { id: 1, nombre: "Auxiliar Administrativo en Salud", cat: "Salud", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 1200000, ini: 150000, c: 5, v: 220000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 1100000, ini: 100000, c: 5, v: 210000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 830000, ini: 100000, c: 5, v: 210000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 1200000, ini: 150000, c: 5, v: 220000 }, { t: "Sábados 7 am a 5 pm", total: 1590000, ini: 250000, c: 5, v: 278000 }, { t: "Domingos 7 am a 5 pm", total: 1300000, ini: 150000, c: 5, v: 240000 }] },
     { id: 2, nombre: "Atención Integral a la Primera Infancia", cat: "Salud", horarios: [{ t: "Lunes a Viernes 8 am a 12 pm", total: 920000, ini: 120000, c: 5, v: 170000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 700000, ini: 50000, c: 5, v: 140000 }, { t: "Sábados 7 am a 5 pm", total: 1000000, ini: 150000, c: 5, v: 180000 }, { t: "Domingos 7 am a 5 pm", total: 950000, ini: 100000, c: 5, v: 180000 }] },
     { id: 3, nombre: "Auxiliar en Enfermería", cat: "Salud", horarios: [{ t: "Lunes a Viernes 2 pm a 5:30 pm", total: 2650000, ini: 600000, c: 5, v: 430000 }]},
@@ -8,7 +7,6 @@ const programas = [
     { id: 6, nombre: "Auxiliar en Servicios Farmacéuticos", cat: "Salud", horarios: [{ t: "Lunes a Viernes 6 am a 10 am", total: 1600000, ini: 300000, c: 4, v: 260000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 1600000, ini: 300000, c: 4, v: 260000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 1600000, ini: 300000, c: 4, v: 260000 }] },
     { id: 19, nombre: "Acondicionamiento Físico y Entrenamiento Deportivo", cat: "Salud", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 600000, ini: 50000, c: 5, v: 110000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Domingos 7 am a 5 pm", total: 800000, ini: 100000, c: 5, v: 150000 }] },
 
-    // --- ESCUELA DE ADMINISTRACIÓN (7 Programas) ---
     { id: 10, nombre: "Secretariado", cat: "Administracion", horarios: [{ t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }] },
     { id: 12, nombre: "Asistente de Mercadeo", cat: "Administracion", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }] },
     { id: 13, nombre: "Operación de Servicios Turísticos y Hoteleros", cat: "Administracion", horarios: [{ t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }] },
@@ -17,13 +15,11 @@ const programas = [
     { id: 16, nombre: "Asistente en Contabilidad y Finanzas", cat: "Administracion", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 600000, ini: 50000, c: 5, v: 110000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Domingos 7 am a 5 pm", total: 800000, ini: 100000, c: 5, v: 150000 }] },
     { id: 17, nombre: "Asistente en Gestión Humana", cat: "Administracion", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 600000, ini: 50000, c: 5, v: 110000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Domingos 7 am a 5 pm", total: 800000, ini: 100000, c: 5, v: 150000 }] },
 
-    // --- ESCUELA DE COMUNICACIÓN E INFORMÁTICA (4 Programas) ---
     { id: 11, nombre: "Auxiliar en Comunicación Empresarial", cat: "Comunicacion e Informatica", horarios: [{ t: "Sábados 7 am a 5 pm", total: 780000, ini: 130000, c: 5, v: 140000 }] },
     { id: 18, nombre: "Asistente en Servicios Administrativos", cat: "Comunicacion e Informatica", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 600000, ini: 50000, c: 5, v: 110000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Domingos 7 am a 5 pm", total: 800000, ini: 100000, c: 5, v: 150000 }] },
     { id: 31, nombre: "Asistente en Diseño Gráfico", cat: "Comunicacion e Informatica", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 850000, ini: 100000, c: 5, v: 160000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 800000, ini: 100000, c: 5, v: 150000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 880000, ini: 100000, c: 5, v: 166000 }, { t: "Sábados 7 am a 5 pm", total: 1000000, ini: 170000, c: 5, v: 176000 }] },
     { id: 32, nombre: "Asistente en Sistemas", cat: "Comunicacion e Informatica", horarios: [{ t: "Lunes a Viernes 6 am a 8 am", total: 800000, ini: 120000, c: 5, v: 146000 }, { t: "Lunes a Viernes 8 am a 12 pm", total: 720000, ini: 70000, c: 5, v: 140000 }, { t: "Lunes a Viernes 2 pm a 5:30 pm", total: 600000, ini: 50000, c: 5, v: 110000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 780000, ini: 100000, c: 5, v: 146000 }, { t: "Sábados 7 am a 5 pm", total: 900000, ini: 100000, c: 5, v: 170000 }, { t: "Domingos 7 am a 5 pm", total: 800000, ini: 100000, c: 5, v: 150000 }] },
 
-    // --- BACHILLERATO (1 Programa) ---
     { id: 40, nombre: "Bachillerato", cat: "Bachillerato", horarios: [{ t: "Lunes a Viernes 9 am a 11:15 am", total: 500000, ini: 100000, c: 5, v: 90000 }, { t: "Lunes a Viernes 6 pm a 8 pm", total: 500000, ini: 100000, c: 5, v: 90000 }, { t: "Sábados 7 am a 5 pm", total: 500000, ini: 100000, c: 5, v: 90000 }, { t: "Domingos 7 am a 5 pm", total: 500000, ini: 100000, c: 5, v: 90000 }] }
 ];
 
@@ -59,7 +55,6 @@ function updateUI(el, id) {
     const ctx = document.getElementById(`price-ctx-${id}`);
     if(!ctx) return;
     
-    // El texto dentro de la caja de pago también suele verse bien centrado
     ctx.innerHTML = `
         <div style="text-align: center;">
             <div class="monto-inicial">Inicial: $${h.ini.toLocaleString()}</div>
